433dd8786f8c354498fb76bf4f484f10a36f59e4
kamalcodezen <kamaluddin7908@gmail.com>
2026-05-13 03:52:39 +0530

    onClick Handle explore

--- src/components/ListGroup.tsx
+++ src/components/ListGroup.tsx
@@ -1,12 +1,17 @@
+import { MouseEvent } from "react";
+
 const ListGroup = () => {
   const cities = ["kolkata", "Delhi", "mumbai", "Up"];
+
+  const handleClick = (event: MouseEvent) => console.log(event);
 
   return (
     <>
       <h1>List</h1>
+      {cities.length === 0 && <p>Cities Not Fund</p>}
       <ul className="list-group">
         {cities.map((city, i) => (
-          <li key={i} className="list-group-item">
+          <li key={i} onClick={handleClick} className="list-group-item">
             {city}
           </li>
         ))}
