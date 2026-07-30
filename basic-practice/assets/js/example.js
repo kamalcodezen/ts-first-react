1abebc365ffde53f11309155c9c597f543c17a65
kamalcodezen <kamaluddin7908@gmail.com>
2026-07-30 17:39:59 +0530

    Type checking

---
+++ basic-practice/assets/js/example.js
@@ -0,0 +1,7 @@
+"use strict";
+const fullName = "Praveen";
+console.log(fullName);
+const age = 24;
+console.log(age);
+const person = "Aisha";
+console.log(person);
---
+++ basic-practice/assets/ts/example.ts
@@ -0,0 +1,8 @@
+const fullName: string = "Praveen";
+console.log(fullName);
+
+const age: number = 24;
+console.log(age);
+
+const person: string = "Aisha";
+console.log(person);
